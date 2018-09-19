95226aec55f5b46c67a5ef42eb559381ec070412
CraftyCrashers <42997537+CraftyCrashers@users.noreply.github.com>
2018-09-19 14:06:31 +0200

    Transformaties

---
+++ 04 - Variabelen/Transformaties.py
@@ -0,0 +1,10 @@
+a = int(input('Geef het waarde van a'))
+b = int(input('Geef het waarde van b'))
+x1 = '2(x - '
+x2 = ')^2 + '
+f1 = 'f(x) = '
+g1 = 'g(x) = '
+f = f1 + x1 + str(3) + x2 + str(4)
+print(f)
+g = g1 + x1 + str(3 + a) + x2 + str(4 + b)
+print(g)
