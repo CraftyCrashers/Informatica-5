175b039b45914f6d3137b2fa7bdb988e155dc27a
CraftyCrashers <42997537+CraftyCrashers@users.noreply.github.com>
2018-09-19 22:10:34 +0200

    De gestopte klok

---
+++ Uitbreiding/04 + Variablen/Graansilo vullen.py
@@ -0,0 +1,16 @@
+b = float(input('Wat is het breedte van het graanveld?'))
+l = float(input('Wat is het lengte van het graanveld?'))
+c = float(input('Hoeveel graan in kubieke meter per hectare?'))
+r = float(input('Wat is de straal van de graansilo?'))
+h = float(input('Wat is de hoogte van de graansilo?'))
+
+aantalGraan = ((b * l) / (10 ** 4)) * c
+capaciteitSilo = r * r * h * 3.1415926535897931
+voldoendeSilo = int(aantalGraan / capaciteitSilo)
+overigGraan = (aantalGraan % capaciteitSilo) / capaciteitSilo
+overigGraan *= h
+
+print(voldoendeSilo)
+print(overigGraan)
+print(aantalGraan)
+print(capaciteitSilo)
