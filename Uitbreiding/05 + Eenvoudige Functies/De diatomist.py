c834db2306d200176190f68bace5efcea383cf8f
CraftyCrashers <42997537+CraftyCrashers@users.noreply.github.com>
2018-09-26 14:18:31 +0200

    De diatomist

---
+++ Uitbreiding/05 + Eenvoudige Functies/De diatomist.py
@@ -0,0 +1,12 @@
+#Invoer 2 stralen
+r = float(input('Geef de straal van de kleine cirkel'))
+R = float(input('Geef de straal van de grote cirkel'))
+
+#Formule voor maximaal aantal kleine cirkels
+n = int(0.83*((R * R) / (r * r)) - 1.9)
+
+#Bedekkingsgraad
+bedekkingsgraad = ((r * r * n) / (R * R) * 100)
+
+#Uitvoer
+print(str(n) + ' kleine cirkels bedekken ' + str(round(bedekkingsgraad, 2)) +'% van de grote cirkel')
