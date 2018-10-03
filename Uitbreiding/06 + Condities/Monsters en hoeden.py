87f9ed63bc7a89dcb618a2e9cf25990c077b28d7
CraftyCrashers <42997537+CraftyCrashers@users.noreply.github.com>
2018-10-03 16:28:13 +0200

    Kleurenblind

--- Uitbreiding/06 + Condities/Monsters en hoeden.py
+++ Uitbreiding/06 + Condities/Monsters en hoeden.py
@@ -12,13 +12,17 @@ wit = 'wit'
 # Puzzelwerk
 
 if kleurBlind == 1 and p2 == zwart:
+    p2 = p1
     p1 = wit
 elif kleurBlind == 1 and p2 == wit:
+    p2 = p1
     p1 = zwart
 elif kleurBlind == 2 and p1 == zwart:
+    p1 = p2
     p2 = wit
 else:
     p2 = zwart
+    p1 = p2
 
 #Wat zegt de mannen
 
