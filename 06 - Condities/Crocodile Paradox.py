b242ec3a9bb2d094fb5ee77aa2ed08de2c0f0a1c
CraftyCrashers <42997537+CraftyCrashers@users.noreply.github.com>
2018-10-03 11:32:19 +0200

    Krokodil?

---
+++ 06 - Condities/Crocodile Paradox.py
@@ -0,0 +1,10 @@
+# invoer
+x = input('Wie heeft stalen zenuwen?')
+
+# Booleans en uitvoer
+if x == 'vader':
+    print('Krokodil geeft kind terug')
+elif x == 'krokodil':
+    print('Krokodil eet kind op')
+else:
+    print('Moe van het denken')
