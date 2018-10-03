34140a2a582e44e00293593e1f7a8f3d0377cb03
CraftyCrashers <42997537+CraftyCrashers@users.noreply.github.com>
2018-10-03 17:13:38 +0200

    Redundancy is magic

---
+++ Testing and Funhouse/Hello World (but more redundant).py
@@ -0,0 +1,35 @@
+# IIIII          IIIII     IIIIIIIIIIIIIIIIIIII     IIIII                    IIIII                    IIIIIIIIIIIIIIII
+# IIIII          IIIII     IIIIIIIIIIIIIIIIIIII     IIIII                    IIIII                   IIIIIIIIIIIIIIIIII
+# IIIII          IIIII     IIIIIIIIIIIIIIIIIIII     IIIII                    IIIII                  IIIIIIIIIIIIIIIIIIII
+# IIIII          IIIII     IIIII                    IIIII                    IIIII                  IIIII          IIIII
+# IIIII          IIIII     IIIII                    IIIII                    IIIII                  IIIII          IIIII
+# IIIII          IIIII     IIIII                    IIIII                    IIIII                  IIIII          IIIII
+# IIIII          IIIII     IIIII                    IIIII                    IIIII                  IIIII          IIIII
+# IIIIIIIIIIIIIIIIIIII     IIIII                    IIIII                    IIIII                  IIIII          IIIII
+# IIIIIIIIIIIIIIIIIIII     IIIIIIIIIIIIIIIIIIII     IIIII                    IIIII                  IIIII          IIIII
+# IIIIIIIIIIIIIIIIIIII     IIIIIIIIIIIIIIIIIIII     IIIII                    IIIII                  IIIII          IIIII
+# IIIIIIIIIIIIIIIIIIII     IIIIIIIIIIIIIIIIIIII     IIIII                    IIIII                  IIIII          IIIII
+# IIIII          IIIII     IIIII                    IIIII                    IIIII                  IIIII          IIIII
+# IIIII          IIIII     IIIII                    IIIII                    IIIII                  IIIII          IIIII
+# IIIII          IIIII     IIIII                    IIIII                    IIIII                  IIIII          IIIII
+# IIIII          IIIII     IIIII                    IIIII                    IIIII                  IIIII          IIIII
+# IIIII          IIIII     IIIII                    IIIII                    IIIII                  IIIII          IIIII
+# IIIII          IIIII     IIIII                    IIIII                    IIIII                  IIIII          IIIII
+# IIIII          IIIII     IIIIIIIIIIIIIIIIIIII     IIIIIIIIIIIIIIIIIIII     IIIIIIIIIIIIIIIIIIII   IIIIIIIIIIIIIIIIIIII
+# IIIII          IIIII     IIIIIIIIIIIIIIIIIIII     IIIIIIIIIIIIIIIIIIII     IIIIIIIIIIIIIIIIIIII    IIIIIIIIIIIIIIIIII
+# IIIII          IIIII     IIIIIIIIIIIIIIIIIIII     IIIIIIIIIIIIIIIIIIII     IIIIIIIIIIIIIIIIIIII     IIIIIIIIIIIIIIII
+
+eersteLetter = 'h'
+tweedeLetter = 'E'
+derdeLetter = 'L'
+vierdeLetter = 'L'
+vijfdeLetter = 'O'
+spaceBar1 = ' '
+zesdeLetter = 'w'
+zevendeLetter = 'O'
+achtsteLetter = 'R'
+negendeLetter = 'L'
+tiendeLetter = 'D'
+if str.upper(eersteLetter) + str.lower(tweedeLetter) + str.lower(derdeLetter) + str.lower(vierdeLetter) + str.lower(vijfdeLetter) + str.capitalize(spaceBar1) + str.upper(zesdeLetter) + str.lower(zevendeLetter) + str.lower(achtsteLetter) + str.lower(negendeLetter) + str.lower(tiendeLetter) == 'Hello World':
+    HelloWorld = str.upper(eersteLetter) + str.lower(tweedeLetter) + str.lower(derdeLetter) + str.lower(vierdeLetter) + str.lower(vijfdeLetter) + str.capitalize(spaceBar1) + str.upper(zesdeLetter) + str.lower(zevendeLetter) + str.lower(achtsteLetter) + str.lower(negendeLetter) + str.lower(tiendeLetter)
+print('{}{}{}{}{}{}{}{}{}{}{}'.format(str.upper(eersteLetter), str.lower(tweedeLetter), str.lower(derdeLetter), str.lower(vierdeLetter), str.lower(vijfdeLetter), str.capitalize(spaceBar1), str.upper(zesdeLetter), str.lower(zevendeLetter), str.lower(achtsteLetter), str.lower(negendeLetter), str.lower(tiendeLetter)))
