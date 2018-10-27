a04e094ccc66d8d4c4f603f95f25cab1aa808e63
CraftyCrashers <42997537+CraftyCrashers@users.noreply.github.com>
2018-10-27 09:30:02 +0200

    Am I in control? - Flittzy 2018

---
+++ 07b - Iteraties - For-lus/Blackjack.py
@@ -0,0 +1,16 @@
+# Format
+
+nummer = 1
+totaal = 0
+while nummer != 0 and totaal < 21:
+    nummer = int(input('Waarde kaart?: '))
+    totaal += nummer
+
+# Resultaat
+
+if totaal == 21:
+    print('Gewonnnen!')
+elif totaal > 21:
+    print('Verbrand ({})'.format(totaal))
+else:
+    print('Voorzichtig gespeeld ({})'.format(totaal))
