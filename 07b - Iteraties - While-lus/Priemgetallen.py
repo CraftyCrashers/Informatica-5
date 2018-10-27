190575612b5f12817b77a38578621a9f975bc02d
CraftyCrashers <42997537+CraftyCrashers@users.noreply.github.com>
2018-10-27 13:51:24 +0200

    I like money - Mr. Krabs

---
+++ 07b - Iteraties - While-lus/Priemgetallen.py
@@ -0,0 +1,16 @@
+# Input
+
+getal = int(input('Geef een getal.: '))
+
+# Format
+
+deler = 2
+while getal / deler != getal // deler and getal != 0 and getal != 1:
+    deler += 1
+
+# Uitvoer
+
+if getal == deler:
+    print(str(getal) + ' is een priemgetal')
+else:
+    print(str(getal) + ' is geen priemgetal')
