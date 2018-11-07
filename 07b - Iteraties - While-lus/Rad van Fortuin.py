139137bcfccd50998597b71601a2587f2b6dd241
CraftyCrashers <42997537+CraftyCrashers@users.noreply.github.com>
2018-11-07 10:36:52 +0100

    Fortuin waard

--- 07b - Iteraties - While-lus/Rad van Fortuin.py
+++ 07b - Iteraties - While-lus/Rad van Fortuin.py
@@ -7,7 +7,7 @@ letter = input('Welke letter bevat het woord?: ')
 # Format
 
 l, totaal, geraden_letters = 0, 0, ''
-while letter in woord and l not in geraden_letters:
+while letter in woord and letter not in geraden_letters:
     totaal += geldbedrag
     geraden_letters += letter
     letter = input('Welke letter bevat het woord?: ')
