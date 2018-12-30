4cb916fe099f9447f35a01c119d0fa38911b3574
CraftyCrashers <42997537+CraftyCrashers@users.noreply.github.com>
2018-12-30 16:47:27 +0100

    Tricky

--- Uitbreiding/07b + Iteraties - While-lus/Hittegolf.py
+++ Uitbreiding/07b + Iteraties - While-lus/Hittegolf.py
@@ -1,16 +1,23 @@
 # Format
 x = input('Graden celcius?: ')
-y = 0
+y, z, X = 0, 0, 'stop'
 aanwezigheid = 'geen hittegolf'
 while x != 'stop':
     x = float(x)
     if x >= 25:
         y += 1
-        if y >= 5:
+        if x >= 30:
+            z += 1
+        x = input('Graden celcius?: ')
+        if y >= 5 and z >= 3:
+            x, X = 'stop', 'null'
             aanwezigheid = 'hittegolf'
     else:
-        y = 0
-    x = input('Graden celcius?: ')
+        y, z = 0, 0
+        x = input('Graden celcius?: ')
+
+while x == 'stop' and X != 'stop':
+    X = input('Typ gewoon stop.')
 
 # Uitvoer
 print(aanwezigheid)
