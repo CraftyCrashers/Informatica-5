0eadbc627e83d8f47194c9ae8e57e1975101d1a8
CraftyCrashers <42997537+CraftyCrashers@users.noreply.github.com>
2019-01-03 17:07:28 +0100

    My gosh that took forever

--- 07c - Iteraties - GenesteLussen/Som eerste n priemgetallen.py
+++ 07c - Iteraties - GenesteLussen/Som eerste n priemgetallen.py
@@ -1,18 +1,20 @@
 # Input
-n = int(input('Aantal priemgetallen: '))
+n = int(input('Aantal priemgetallen: ')) - 1
 
 # Proces
-huidigAantalPriemgetal, huidigPriemgetal, huidigDeler, somPriemgetallen, x = 0, 2, 2, 0, 0
+huidigAantalPriemgetal, huidigPriemgetal, huidigDeler, somPriemgetallen, x = 0, 3, 2, 2, 0
 while huidigAantalPriemgetal < n:
-    for i in range(1, huidigPriemgetal // 2):
-        if huidigPriemgetal / i == huidigPriemgetal // i and x == 0:
-            x = 1
-    if x == 0:
-        somPriemgetallen += huidigPriemgetal
-        huidigAantalPriemgetal += 1
-    else:
-        x = 0
-    huidigPriemgetal += 1
+    while huidigDeler <= huidigPriemgetal:
+        if huidigPriemgetal / huidigDeler == huidigPriemgetal // huidigDeler and huidigPriemgetal != huidigDeler:
+            huidigDeler += huidigPriemgetal
+        elif huidigDeler >= huidigPriemgetal // 2:
+            huidigAantalPriemgetal += 1
+            somPriemgetallen += huidigPriemgetal
+            huidigDeler += huidigPriemgetal
+        else:
+            huidigDeler += 1
+    huidigPriemgetal += 2
+    huidigDeler = 2
 
 # Output
 print(somPriemgetallen)
