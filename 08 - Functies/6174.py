4762878f89af558b8c359b48a52d288807069924
CraftyCrashers <42997537+CraftyCrashers@users.noreply.github.com>
2019-01-09 11:18:54 +0100

    Wow, this requires thinking.

--- 08 - Functies/6174.py
+++ 08 - Functies/6174.py
@@ -7,37 +7,39 @@ def splits(a):
 
 
 def oplopende_cijfers(a, b, c, d):
-    x2, x3 = 0, 10
     x1 = min(a, b, c, d)
     x4 = max(a, b, c, d)
-    for i in a, b, c, d:
-        if x1 < i < x4 and i < x3 and x2 == 0:
-            x2 = i
-        elif x1 < i < x4 and i < x2:
-            x3 = x2
-            x2 = i
-        elif x1 < i < x4 and i > x2:
-            x3 = i
+    k2 = max(min(a, b), min(a, c), min(b, c))
+    k3 = a + b + c + d - x1 - k2 - x4
+    x2 = min(k3, k2)
+    x3 = max(k3, k2)
     return x1, x2, x3, x4
+
+# Makkelijker
+# k2 = max(min(a, b), min(a, c), min(b, c))
+# k3 = a + b + c + d - x1 - k2 - x4
+# x2 = min(k3, k2)
+# x3 = max(k3, k2)
 
 
 def op_af_getallen(a, b, c, d):
-    x1 = a * 1000 + b * 100 + c * 10 + d
-    x2 = d * 1000 + c * 100 + b * 10 + a
+    x1 = str(a) + str(b) + str(c) + str(d)
+    x2 = x1[::-1]
     return x1, x2
+
+# Tip: gebruik stringfuncties
 
 
 def verschil(a, b):
-    a, b = int(a), int(b)
-    return a - b
+    return str(int(a) - int(b))
 
 
 def kaprekar(a):
-    x1 ,x2, x3, x4 = splits(a)
-    x1, x2, x3, x4 = oplopende_cijfers(x1, x2, x3, x4)
-    kleinste, grootste = op_af_getallen(x1, x2, x3, x4)
-    a = verschil(grootste, kleinste)
-    return'{} - {} = {}\n'.format(grootste, kleinste, a)
-
-
-print(kaprekar(5342))
+    bewerkingen = ''
+    while a != 6174:
+        x1, x2, x3, x4 = splits(a)
+        x1, x2, x3, x4 = oplopende_cijfers(x1, x2, x3, x4)
+        kleinste, grootste = op_af_getallen(x1, x2, x3, x4)
+        a = int(verschil(grootste, kleinste))
+        bewerkingen += str(grootste) + ' - ' + str(kleinste) + ' = ' + str(a) + '\n'
+    return bewerkingen[:-1]
