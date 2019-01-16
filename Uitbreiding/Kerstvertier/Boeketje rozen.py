32e9c764b4ead4cb36f86724545e2062a31a5fd8
CraftyCrashers <42997537+CraftyCrashers@users.noreply.github.com>
2019-01-16 11:40:41 +0100

    Gewoon info

---
+++ Uitbreiding/Kerstvertier/Boeketje rozen.py
@@ -0,0 +1,8 @@
+# Info invoer: 1. # rode en witte rozen
+#              2. # blauwe en witte rozen
+#              3. # blauwe en rode rozen > / < # witte en blauwe roze
+# Info Uitvoer: 1. # Blauwe rozen, 2. # Witte rozen, 3. # Rode rozen
+# Voorbeeld:
+# 100 # rode en witte rozen
+# 53 # blauwe en witte rozen
+# < # blauwe en rode rozen kleiner dan # witte en blauwe roze
