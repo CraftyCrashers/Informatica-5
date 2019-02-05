cff01ba8f5e0c04f8e91a1bd972ecce3ad3d41ec
CraftyCrashers <42997537+CraftyCrashers@users.noreply.github.com>
2019-02-05 08:07:05 +0100

    Een correcte upgrade

--- 12a - Lists/Gotcha.py
+++ 12a - Lists/Gotcha.py
@@ -1,7 +1,7 @@
 def ik_heb_gemoord(opdrachten_lijst, moordenaar):
     volgend_doelwit = opdrachten_lijst[(opdrachten_lijst.index(moordenaar) + 2) % len(opdrachten_lijst)]
     if not len(opdrachten_lijst) == 1:
-        opdrachten_lijst.pop(opdrachten_lijst.index(moordenaar) + 1 % len(opdrachten_lijst))
+        opdrachten_lijst.pop((opdrachten_lijst.index(moordenaar) + 1) % len(opdrachten_lijst))
     return volgend_doelwit, opdrachten_lijst
 
 
