7623f0b8a8210d944f0b5714f5655e2f2d0cdc37
CraftyCrashers <42997537+CraftyCrashers@users.noreply.github.com>
2019-02-08 09:31:58 +0100

    Pls Ignore

--- 12b - Roosters/Kleurendriekhoek.py
+++ 12b - Roosters/Kleurendriekhoek.py
@@ -2,14 +2,21 @@
 def volgende_rij(a):
     nieuw_rij = []
     for i in range(1, len(a)):
+        sub = abs(ord(a[i - 1]) - ord(a[i]))
         if a[i] == a[i - 1]:
             nieuw_rij += a[i]
+        elif sub == 11:
+            nieuw_rij += ['Y']
+        elif sub == 7:
+            nieuw_rij += ['G']
         else:
-            sub = abs(ord(a[i - 1]) - ord(a[i]))
-            if sub == 11:
-                nieuw_rij += ['Y']
-            elif sub == 7:
-                nieuw_rij += ['G']
-            else:
-                nieuw_rij += ['R']
+            nieuw_rij += ['R']
     return nieuw_rij
+
+
+def driehoek(a):
+    geen_VISUALISATIE = [a]
+    for i in range(len(a) - 1):
+        geen_VISUALISATIE = volgende_rij(geen_VISUALISATIE)
+        a += geen_VISUALISATIE
+    return a
