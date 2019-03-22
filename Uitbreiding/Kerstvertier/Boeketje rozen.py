08a38ab6ec02fdfab25b701f8fb3ef7a549877e6
CraftyCrashers <42997537+CraftyCrashers@users.noreply.github.com>
2019-03-22 08:51:50 +0100

    Update

--- Uitbreiding/Kerstvertier/Boeketje rozen.py
+++ Uitbreiding/Kerstvertier/Boeketje rozen.py
@@ -1,12 +1,12 @@
 rood_wit = int(input("Aantal rode en witte rozen: "))
 blauw_wit = int(input("Aantal blauwe en witte rozen: "))
 operator = input("rood groter/kleiner dan wit")
-if blauw_wit - 2 + blauw_wit - 4 == rood_wit:
+if (blauw_wit - 6 + blauw_wit == rood_wit):
     blauw, wit, rood = 2, blauw_wit - 2, blauw_wit - 4
+elif blauw_wit - 6 + blauw_wit + 1 == rood_wit:
+    blauw, wit, rood = 2, blauw_wit - 2, blauw_wit - 3
 elif operator == '<':
     blauw, wit, rood = blauw_wit - rood_wit + 2, rood_wit - 2, 2
-elif rood_wit > blauw_wit:
-    blauw, wit, rood = rood_wit - 2, 2, blauw_wit - 2
 else:
     blauw, wit, rood = blauw_wit - 2, 2, rood_wit - 2
 print(blauw)
